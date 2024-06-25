a936659f2cfdb72ea0e4290d12894536f19a61b9
joao-abrantes-dev <joao.abrantes.dev@gmail.com>
2024-06-25 08:03:07 -0300

    Feature: Lógica dos resultados.

---
+++ js/script.js
@@ -0,0 +1,27 @@
+function calcular() {
+    const homens = parseInt(document.getElementById('homens').value);
+    const mulheres = parseInt(document.getElementById('mulheres').value);
+    const criancas = parseInt(document.getElementById('criancas').value);
+
+
+    const consumo = {
+        carne: { homens: 500, mulheres: 300, criancas: 200 },
+        frango: { homens: 200, mulheres: 200, criancas: 100 },
+        linguica: { homens: 200, mulheres: 200, criancas: 200 },
+        refrigerante: { homens: 300, mulheres: 400, criancas: 200 },
+        cerveja: { homens: 800, mulheres: 500, criancas: 0 }
+    };
+
+    const totalCarne = homens * consumo.carne.homens + mulheres * consumo.carne.mulheres + criancas * consumo.carne.criancas;
+    const totalFrango = homens * consumo.frango.homens + mulheres * consumo.frango.mulheres + criancas * consumo.frango.criancas;
+    const totalLinguica = homens * consumo.linguica.homens + mulheres * consumo.linguica.mulheres + criancas * consumo.linguica.criancas;
+    const totalRefrigerante = homens * consumo.refrigerante.homens + mulheres * consumo.refrigerante.mulheres + criancas * consumo.refrigerante.criancas;
+    const totalCerveja = homens * consumo.cerveja.homens + mulheres * consumo.cerveja.mulheres;
+
+
+    document.getElementById('carne').innerText = `Carne bovina: ${totalCarne}g`;
+    document.getElementById('frango').innerText = `Frango: ${totalFrango}g`;
+    document.getElementById('linguica').innerText = `Linguiça: ${totalLinguica}g`;
+    document.getElementById('refrigerante').innerText = `Refrigerante: ${totalRefrigerante}ml`;
+    document.getElementById('cerveja').innerText = `Cerveja: ${totalCerveja}ml`;
+}
